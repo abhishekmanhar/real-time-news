197f1b4c8bb5d3272e2b853d54111622889346b0
Abhishek Manhar <abhimanharkhan25@gmail.com>
2025-02-22 21:34:18 +0530

    routes updated

--- backend/routes/newsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getNews, postNews } from "../controllers/newsController.js";
-
-const router = express.Router();
-
-router.get("/", getNews);
-router.post("/", postNews);
-
-export default router; // ✅ Add default export
